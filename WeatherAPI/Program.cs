df351bb377561004239419ef37a1b850a0becbdc
Chris Adeyemi <x00198887@mytudublin.ie>
2026-08-13 14:22:59 +0100

    Add Dockerfile for Railway

--- WeatherAPI/Program.cs
+++ WeatherAPI/Program.cs
@@ -3,6 +3,10 @@ using WeatherAPI.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Railway needs the app to listen on its assigned PORT
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
+
 builder.Services.AddControllers();
 
 builder.Services.AddDbContext<WeatherContext>(options =>
